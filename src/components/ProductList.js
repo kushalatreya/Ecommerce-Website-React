a28d83fb5113cf76b12c931a5f840d7fa13febae
kushal atreya <kushalatreya@live.com>
2019-02-03 14:52:39 +0200

    created all components, router is set, bootstraps cdn and fonts added

---
+++ src/components/ProductList.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import Product from './Product'
+export default class ProductList extends Component {
+  render() {
+    return (
+      <div>
+        <h3>hello from ProductList</h3>
+      </div>
+    )
+  }
+}
