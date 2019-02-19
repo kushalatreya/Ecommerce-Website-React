95c3cbe42cc609ac11480c58407271992acef4b7
kushal atreya <kushalatreya@live.com>
2019-02-19 12:43:25 +0200

    span is added to make spacing between name and title

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,18 +1,29 @@
-import React, { Component } from 'react';
-import Product from './Product';
-import {ButtonContainer} from './StyledComponent'
+import React, { Component } from "react";
+import Product from "./Product";
+import Title from "./Title";
+import { ProductWrapper} from './StyledComponent'
+import { ProductConsumer } from "../context";
 export default class ProductList extends Component {
-  state={
-    Product:[]
-  }
+
   render() {
     return (
       <React.Fragment>
-        <div className="py-5">This page is under construction.</div>
-        <ButtonContainer>
-          submit
-        </ButtonContainer>
+        <ProductWrapper className="py-5">
+          <div className="container">
+            <Title name="our" title="products" />
+            <div className="row">
+              <ProductConsumer>
+                {value => {
+                  return value.products.map(product => {
+                    return <Product key={product.id} product={product} />;
+                  });
+                }}
+              </ProductConsumer>
+            </div>
+          </div>
+        </ProductWrapper>
       </React.Fragment>
-    )
+    );
   }
 }
+
